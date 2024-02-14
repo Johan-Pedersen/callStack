8fa39e75087556efa8ffb4a36b6b7b886fa1aafa
Johan-Pedersen <sandbkpedersen.johan@gmail.com>
2024-02-14 07:47:35 +0100

    Only close current callstack

--- lua/callStack/callStack.lua
+++ lua/callStack/callStack.lua
@@ -1,12 +1,26 @@
 local function openWindow()
   WindowHandle = vim.api.nvim_open_win(BufferHandle, true,
     {relative="win", row=5, col=45, width=100, height=30})
+
+  table.insert(Stack, WindowHandle)
 end
 
 local function closeWindow ()
-  vim.api.nvim_win_hide(WindowHandle)
+
+  local current_winID = vim.api.nvim_call_function("win_getid", {})
+
+  for i,winID in ipairs(Stack) do
+
+    if (winID == current_winID) then
+      vim.api.nvim_win_hide(winID)
+      table.remove(Stack, i)
+    end
+
+  end
+
 end
 
+--Set keymap -----------------------
 vim.keymap.set("n", "m", function ()
   openWindow()
 end)
