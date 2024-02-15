26a2c1f1f4d89296eb87ba52ad7a3e37a0cd511a
Johan-Pedersen <sandbkpedersen.johan@gmail.com>
2024-02-15 07:18:40 +0100

    rename Stack -> Windows

--- lua/callStack.lua
+++ lua/callStack.lua
@@ -4,6 +4,6 @@ WindowHandle = nil
 local rootDir = vim.fn.finddir(".git/..", vim.fn.getcwd())
 BufferHandle = vim.fn.bufadd(rootDir..'/callStack.md')
 
-Stack = {}
+Windows = {}
 
 require("callStack.callStack")
--- lua/callStack/callStack.lua
+++ lua/callStack/callStack.lua
@@ -2,18 +2,18 @@ local function openWindow()
   WindowHandle = vim.api.nvim_open_win(BufferHandle, true,
     {relative="win", row=5, col=45, width=100, height=30})
 
-  table.insert(Stack, WindowHandle)
+  table.insert(Windows, WindowHandle)
 end
 
 local function closeWindow ()
 
   local current_winID = vim.api.nvim_call_function("win_getid", {})
 
-  for i,winID in ipairs(Stack) do
+  for i,winID in ipairs(Windows) do
 
     if (winID == current_winID) then
       vim.api.nvim_win_hide(winID)
-      table.remove(Stack, i)
+      table.remove(Windows, i)
     end
 
   end
