73954f8b7c977ecc58119a70d67530243ef360a5
Johan-Pedersen <sandbkpedersen.johan@gmail.com>
2024-04-06 07:43:38 +0200

    Refactor: rename callstack/callstack.lua -> winFuncs Move setkeymap to callStack.lua

--- lua/callStack.lua
+++ lua/callStack.lua
@@ -4,4 +4,16 @@ ThoughtsBuf = vim.fn.bufadd(rootDir..'/Thoughts.md')
 
 Windows = {}
 
-require("callStack.callStack")
+local winFuncs = require("callStack.winFuncs")
+--
+--Set keymap -----------------------
+vim.keymap.set("n", "m", function ()
+  winFuncs.openWindow()
+end)
+vim.keymap.set("n", "<C-m>",function ()
+  winFuncs.closeWindow()
+end )
+
+vim.keymap.set("n", "<C-j>", function()
+  winFuncs.follow()
+end)
--- lua/callStack/winFuncs.lua
+++ lua/callStack/winFuncs.lua
@@ -1,11 +1,13 @@
-local function openWindow()
+local M = {}
+
+function M.openWindow()
   local windowHandle = vim.api.nvim_open_win(CSBuf, true,
     {relative="win", row=5, col=45, width=100, height=30})
 
   table.insert(Windows, windowHandle)
 end
 
-local function closeWindow ()
+function M.closeWindow ()
 
   print("closeWindow")
   local current_winID = vim.api.nvim_call_function("win_getid", {})
@@ -21,17 +23,17 @@ local function closeWindow ()
 
 end
 
-local function follow ()
+function M.follow ()
 
   print("follow")
 
   local current_winID = vim.api.nvim_call_function("win_getid", {})
 
-  for i,winID in ipairs(Windows) do
+  for _,winID in ipairs(Windows) do
 
     if (winID == current_winID) then
 
-      closeWindow()
+      M.closeWindow()
 
       local windowHandle = vim.api.nvim_open_win(ThoughtsBuf, true,
       {relative="win", row=5, col=45, width=100, height=30})
@@ -41,14 +43,4 @@ local function follow ()
   end
 end
 
---Set keymap -----------------------
-vim.keymap.set("n", "m", function ()
-  openWindow()
-end)
-vim.keymap.set("n", "<C-m>",function ()
-  closeWindow()
-end )
-
-vim.keymap.set("n", "<C-j>", function()
-  follow()
-end)
+return M
